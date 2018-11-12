168d2f0ff87c2bc620fa34566dad346617ab3d32
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2018-11-13 00:09:02 +0700

    Add files via upload

--- data/products.js
+++ data/products.js
@@ -19,7 +19,7 @@ var list_products =
 		"rateCount": 0,
 		"promo": {
 			"name": "tragop",
-			"value": "2"
+			"value": "0"
 		}
 	},
 	{
@@ -65,15 +65,158 @@ var list_products =
 			"name": "giareonline",
 			"value": "27.990.000"
 		}
-	},		
+	},
 	{
-		"name": "Xiaomi Mi 8 Lite",
-		"img": "img/products/xiaomi-mi-8-lite-black-1-600x600.jpg",
-		"price": "6.690.000",
-		"star": 4,
-		"rateCount": 1547154,
+		"name": "Oppo A3s 32GB",
+		"img": "img/products/oppo-a3s-32gb-600x600.jpg",
+		"price": "4.690.000",
+		"star": 0,
+		"rateCount": 0,
 		"promo": {
-			"name": "moiramat",
+			"name": "tragop",
+			"value": "0"
+		}
+	},
+	{
+		"name": "Samsung Galaxy J8",
+		"img": "img/products/samsung-galaxy-j8-600x600-600x600.jpg",
+		"price": "6.290.000",
+		"star": 0,
+		"rateCount": 0,
+		"promo": {
+			"name": "giamgia",
+			"value": "500.000"
+		}
+	},
+	{
+		"name": "iPad 2018 Wifi 32GB",
+		"img": "img/products/ipad-wifi-32gb-2018-thumb-600x600.jpg",
+		"price": "8.990.000",
+		"star": 0,
+		"rateCount": 0,
+		"promo": {
+			"name": "tragop",
+			"value": "0"
+		}
+	},
+	{
+		"name": "iPhone 7 Plus 32GB",
+		"img": "img/products/iphone-7-plus-32gb-hh-600x600.jpg",
+		"price": "17.000.000",
+		"star": 0,
+		"rateCount": 0,
+		"promo": {
+			"name": "giareonline",
+			"value": "16.990.000"
+		}
+	},
+	{
+		"name": "Xiaomi Mi 8",
+		"img": "img/products/xiaomi-mi-8-1-600x600.jpg",
+		"price": "12.990.000",
+		"star": 0,
+		"rateCount": 0,
+		"promo": {
+			"name": "",
+			"value": "0"
+		}
+	},
+	{
+		"name": "Mobiistar X",
+		"img": "img/products/mobiistar-x-3-600x600.jpg",
+		"price": "3.490.000",
+		"star": 4,
+		"rateCount": 16,
+		"promo": {
+			"name": "tragop",
+			"value": "0"
+		}
+	},
+	{
+		"name": "Mobiistar Zumbo S2 Dual",
+		"img": "img/products/mobiistar-zumbo-s2-dual-300x300.jpg",
+		"price": "2.850.000",
+		"star": 4,
+		"rateCount": 104,
+		"promo": {
+			"name": "",
+			"value": ""
+		}
+	},
+	{
+		"name": "Mobiistar E Selfie",
+		"img": "img/products/mobiistar-e-selfie-300-1copy-600x600.jpg",
+		"price": "2.490.000",
+		"star": 4,
+		"rateCount": 80,
+		"promo": {
+			"name": "",
+			"value": ""
+		}
+	},
+	{
+		"name": "Mobiistar B310",
+		"img": "img/products/mobiistar-b310-orange-600x600.jpg",
+		"price": "260.000",
+		"star": 5,
+		"rateCount": 87,
+		"promo": {
+			"name": "",
+			"value": ""
+		}
+	},
+	{
+		"name": "Xiaomi Redmi Note 5",
+		"img": "img/products/xiaomi-redmi-note-5-pro-600x600.jpg",
+		"price": "5.690.000",
+		"star": 4,
+		"rateCount": 372,
+		"promo": {
+			"name": "",
+			"value": ""
+		}
+	},
+	{
+		"name": "Xiaomi Redmi 5 Plus 4GB",
+		"img": "img/products/xiaomi-redmi-5-plus-600x600.jpg",
+		"price": "4.790.000",
+		"star": 4,
+		"rateCount": 347,
+		"promo": {
+			"name": "",
+			"value": ""
+		}
+	},
+	{
+		"name": "Huawei Mate 20 Pro",
+		"img": "img/products/huawei-mate-20-pro-green-600x600.jpg",
+		"price": "21.990.000",
+		"star": 5,
+		"rateCount": 4,
+		"promo": {
+			"name": "tragop",
+			"value": "0"
+		}
+	},
+	{
+		"name": "Huawei Nova 3",
+		"img": "img/products/huawei-nova-3-2-600x600.jpg",
+		"price": "9.990.000",
+		"star": 4,
+		"rateCount": 22,
+		"promo": {
+			"name": "tragop",
+			"value": "0"
+		}
+	},
+	{
+		"name": "Huawei Y5 2017",
+		"img": "img/products/huawei-y5-2017-300x300.jpg",
+		"price": "1.990.000",
+		"star": 5,
+		"rateCount": 54,
+		"promo": {
+			"name": "",
 			"value": ""
 		}
 	}
--- js/functions.js
+++ js/functions.js
@@ -65,3 +65,44 @@
 		// document.body.scrollTop = 0; // For Safari
 		// document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 	}
+
+function auto_Get_Database() {
+	var ul = document.getElementsByClassName('homeproduct')[0];
+	var li = ul.getElementsByTagName('li');
+	for(var l of li) {
+		var a = li[0].getElementsByTagName('a')[0];
+		// name
+		var name = a.getElementsByTagName('h3')[0].innerHTML;
+		
+		// price
+		var price = a.getElementsByClassName('price')[0]
+		price = price.getElementsByTagName('strong')[0].innerHTML;
+
+		// img
+		var img = a.getElementsByTagName('img')[0];
+			// tao link download
+		var link = document.createElement('a'); 
+		link.href = img.src;
+			// lay ten hinh anh
+		img = img.src.split('/'); 
+		img = img[img.length - 1];
+			// download hinh anh voi ten lay duoc
+		link.download = img; 
+		document.body.appendChild(link);
+		link.click();
+
+		// rating
+		var rating = a.getElementsByClassName('ratingresult')[0];
+		var star = rating.getElementsByClassName('icontgdd-ystar').length;
+		var rateCount = parseInt(rating.getElementsByTagName('span')[0].innerHTML);
+
+		// promo
+		var tragop = a.getElementsByClassName('installment');
+		if(tragop.length) {
+
+		}
+
+		var giamgia = a.getElementsByClassName('discount').length;
+		var giareonline = a.getElementsByClassName('shockprice').length;
+	}
+}
