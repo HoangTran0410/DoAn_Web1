60e4a1b113c318a6421cdab85ec186c20400543d
Hoang Tran <th01634044850@gmail.com>
2018-11-30 22:55:31 +0700

    fix check thong tin

--- js/lienhe.js
+++ js/lienhe.js
@@ -8,41 +8,36 @@ window.onload = function () {
 function nguoidung() {
     //kiem tra ho ten
     var hoten = document.formlh.ht.value;
-    var ktrahoten = isNaN(hoten);
     //kiem tra so dien thoai
     var dienthoai = document.formlh.sdt.value;
-    var ktradienthoai = isNaN(dienthoai);
 
     //kiểm tra họ tên
-    if (ktrahoten == false) {
-        addAlertBox('Mời bạn nhập lại họ tên.', '#f55', '#000');
+    if (!checkName(hoten)) {
+        addAlertBox('Họ tên không phù hợp.', '#f55', '#000', 3000);
         formlh.ht.focus();
         return false;
     }
     //-------
-    else if (ktradienthoai == true) {
-        addAlertBox('Mời bạn nhập số điện thoại.', '#f55', '#000');
-        formlh.sdt.focus();
-        return false;
-    } else if (dienthoai.charAt(0) == 0) {
-        if (dienthoai.length != 10) {
-            addAlertBox('Số điện thoại phải gồm 10 số.', '#f55', '#000');
-            formlh.sdt.focus();
-            return false;
-        }
-        return true;
-    } else if (dienthoai.charAt(0) == "+" && dienthoai.charAt(1) == 8 && dienthoai.charAt(2) == 4) {
-        if (dienthoai.length != 12) {
-            addAlertBox('Số điện thoại không phù hợp.', '#f55', '#000');
-            formlh.sdt.focus();
-            return false;
-        }
-        return true;
-    }
-    if (dienthoai.charAt(0) != 0 || dienthoai.charAt(0) != "+84" || dienthoai.charAt(1) == 0) {
-        addAlertBox('Số điện thoại phải bắt đầu từ 0 hoặc +84.', '#f55', '#000');
-        formlh.sdt.focus();
+    else if (!checkPhone(dienthoai)) {
+        addAlertBox('Số điện thoại không phù hợp.', '#f55', '#000', 3000);
         return false;
     }
-    return true; // nếu không có gì sai thì submit
+
+    addAlertBox('Gửi thành công. Chúng tôi chân thành cám ơn những góp ý từ bạn.', '#5f5', '#000', 5000); // cám ơn
+    // document.formlh.reset(); // làm sạch
+    return false; // thoát
+}
+
+function checkName(str) {
+    for (var i = 0; i < str.length; i++) {
+        if (Number(str[i])) return false;
+    }
+    return true;
+}
+
+function checkPhone(phone) {
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (phone.match(phoneno)) return true;
+
+    return false;
 }
