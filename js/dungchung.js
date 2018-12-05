40c29ceadfb6efc30ea9be9fd46c9b6adc18875d
Hoang Tran <th01634044850@gmail.com>
2018-12-05 16:05:54 +0700

    add icon navbar

--- js/dungchung.js
+++ js/dungchung.js
@@ -456,12 +456,11 @@ function addTopNav() {
             </div> <!-- End Social Topnav -->
 
             <ul class="top-nav-quicklink flexContain">
-                <li><a href="index.html">Trang chủ</a></li>
-                <li><a href="tintuc.html">Tin tức</a></li>
-                <li><a href="tuyendung.html">Tuyển dụng</a></li>
-                <!-- <li><a href="gioithieu.html">Giới thiệu</a></li> -->
-                <li><a href="trungtambaohanh.html">Trung tâm bảo hành</a></li>
-                <li><a href="lienhe.html">Liên hệ</a></li>
+                <li><a href="index.html"><i class="fa fa-home"></i> Trang chủ</a></li>
+                <li><a href="tintuc.html"><i class="fa fa-newspaper-o"></i> Tin tức</a></li>
+                <li><a href="tuyendung.html"><i class="fa fa-handshake-o"></i> Tuyển dụng</a></li>
+                <li><a href="trungtambaohanh.html"><i class="fa fa-wrench"></i> Trung tâm bảo hành</a></li>
+                <li><a href="lienhe.html"><i class="fa fa-phone"></i> Liên hệ</a></li>
             </ul> <!-- End Quick link -->
         </section><!-- End Section -->
     </div><!-- End Top Nav  -->`);
