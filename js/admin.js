52ca52f03b20add60cfc15c9e8505ebf10a92f4c
Hoang Tran <th01634044850@gmail.com>
2018-12-17 23:58:42 +0700

    comment

--- js/admin.js
+++ js/admin.js
@@ -686,10 +686,11 @@ function xoaNguoiDung(taikhoan) {
         var listuser = getListUser();
         for(var i = 0; i < listuser.length; i++) {
             if(listuser[i].username == taikhoan) {
-                listuser.splice(i, 1);
-                setListUser(listuser);
-                localStorage.removeItem('CurrentUser');
-                addTableKhachHang();
+                listuser.splice(i, 1); // xóa
+                setListUser(listuser); // lưu thay đổi
+                localStorage.removeItem('CurrentUser'); // đăng xuất khỏi tài khoản hiện tại (current user)
+                addTableKhachHang(); // vẽ lại bảng khách hàng
+                addTableDonHang(); // vẽ lại bảng đơn hàng
                 return;
             }
         }
